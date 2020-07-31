15d0d6ce0e075eb8e9bbc5fa4973eb77f4eea85e
alissonao <alisson.oliveira@sicoob.com.br>
2020-07-31 16:45:16 -0300

    Formatando os dados para o retorno

--- src/controller/ListarResultado.js
+++ src/controller/ListarResultado.js
@@ -1,6 +1,7 @@
 
 const connection = require('../database/connection');
-
+const { format, parseISO } = require('date-fns');
+const { da } = require('date-fns/locale');
 
 module.exports = {
 
@@ -9,18 +10,32 @@ module.exports = {
         const dataHoraInicio = new Date(data).toISOString().split("T")[0] + " 00:00:00";
         const dataHorarioFim = new Date(data).toISOString().split("T")[0] + " 23:59:59";
         const result = await connection.select(
-            'nomegalgo', 'datainicio', 'datafim', 'Hist_pista_betfair.pista', 'Hist_pista_betfair.grade', 'trap', 'odd_lay', 'odd_back' 
-            ,'probabilidade', 'din_investido AS Total Galgo', 'win', 'total_dinheiro AS total da corrida '
+            'nomegalgo', 'datainicio', 'datafim', 'Hist_pista_betfair.pista', 'Hist_pista_betfair.grade', 'trap', 'odd_lay', 'odd_back'
+            , 'probabilidade', 'din_investido AS Total Galgo', 'win', 'total_dinheiro AS total da corrida '
         ).from('Hist_galgo_betfair')
             .innerJoin('Hist_pista_betfair', 'id_hist_pista', 'Hist_pista_betfair.id')
             .where('Hist_pista_betfair.pais', 'GB')
             .whereBetween('datainicio', [dataHoraInicio, dataHorarioFim])
             .orderBy('datainicio')
-            console.log(result.length)
-        return res.json(result);
 
+        for (item in result) {
+            result[item]['datainicio'] = dataFormata(result[item]['datainicio'])
+            result[item]['datafim'] = dataFormata(result[item]['datafim'])
+            if (result[item]['win'] === true) {
+                result[item]['win'] = "Venceu"
+            } else {
+                result[item]['win'] = "Perdeu"
+            }
+        }
+
+        console.log(result)
+        return res.json(result);
     }
 
 
+}
+
+const dataFormata = (data) => {
+    return format(new Date(data), "H:mm:ss a");
 }
 
