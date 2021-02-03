9bf4fb4c77d9aae3113f60951e0865fd22b39322
alissonao <alisson.oliveira@sicoob.com.br>
2021-02-03 14:01:54 -0300

    tese deploy6

--- src/database/ConnectionMongoDB.js
+++ src/database/ConnectionMongoDB.js
@@ -1,8 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
-const config = require('../../MongConfig');
-const client = new MongoClient(config, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+// const MongoClient = require('mongodb').MongoClient;
+// const config = require('../../MongConfig');
+// const client = new MongoClient(config, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const client = null;
 module.exports = client;
