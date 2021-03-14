94274baa3e60b9b02728780a242035b9dfd7ab0f
AlissonAO <psyalisson@gmail.com>
2021-03-14 15:15:07 -0300

    Update ConsultarCorrida.js
    Ajustando o horario

--- src/controller/ConsultarCorrida.js
+++ src/controller/ConsultarCorrida.js
@@ -10,7 +10,7 @@ const ObjectID = require('mongodb').ObjectID;
 module.exports = {
   async obterProximaCorrida(req, res) {
     let dateAtual = new Date();
-    dateAtual.setHours(dateAtual.getHours());
+   dateAtual.setHours(dateAtual.getHours() - 3);
     const parsedDate = parseISO(dateAtual.toISOString());
     const znDate = zonedTimeToUtc(parsedDate, {
       timeZone: 'America/Sao_Paulo',
